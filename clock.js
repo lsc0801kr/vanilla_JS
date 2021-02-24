2abcf387ba45ede54bff893bdf6d389dfa5496e7
lsc0801kr <74335091+lsc0801kr@users.noreply.github.com>
2021-02-24 22:04:53 +0900

    시간 if문
    만약 시간이 10보다 작을 때 앞에 0을 붙이도록 만들었다.

--- clock.js
+++ clock.js
@@ -6,11 +6,14 @@ const clockContainer = document.querySelector(".js-clock"),
      const minutes = date.getMinutes();
      const hours = date.getHours();
      const seconds = date.getSeconds();
-     clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+         minutes < 10 ? `0${minutes}`:minutes}:${
+             seconds < 10 ? '0&{seconds}' : seconds }`;
+     //시,분,초가 10초 보다 작을 때 앞에 0을 붙힌다.
  }
 
 function init(){
     getTime();
-
+    setInterval(getTime, 1000) //setInterval(시간을 얻는 부분, 초기화 되는 시간 1000이 1초임)
 }
 init()
